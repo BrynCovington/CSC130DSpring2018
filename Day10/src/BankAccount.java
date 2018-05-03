0803c532b77ec3a47bdb5afc403877ab41073152
BrynCovington <38948217+BrynCovington@users.noreply.github.com>
2018-05-03 08:45:10 -0400

    Bank Account $$

---
+++ Day10/src/BankAccount.java
@@ -0,0 +1,11 @@
+/* dont know what to put in this */
+
+public class BankAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+	}
+
+}
---
+++ Day10/src/TestBankAccount.java
@@ -0,0 +1,10 @@
+//Bryn Covington March 6th 2018
+
+public class TestBankAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
